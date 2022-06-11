21511cbe172dbeb15fbdc9b3719eb5ae0e9628a3
g3th <theinkandpen@yahoo.co.uk>
2022-06-11 07:03:55 +0100

    modified:   windscribe.py

--- windscribe.py
+++ windscribe.py
@@ -8,6 +8,9 @@ email = 'youremail' #some temp-email api?
 
 signup = 'https://windscribe.com/signup'
 #page = 'https://res.windscribe.com/res/init'
+
+# Your own credentials here --->
+
 payload = {'signup': '1','username':username,'password':password,'password2': password,'email': email,'voucher_code': '','captcha': '','robert_status': '1','unlimited_plan': '0'}
 
 print('\x1bc')
@@ -17,13 +20,16 @@ titleHeader()
 print('-'*60)
 
 with requests.session() as windscribe:
+	
+	# Your Ipvanish proxy credentials (not the same as login!) --->
+
 	tor = {'http':'socks5h://user:pass@tor.socks.ipvanish.com:1080'}
 	myip = windscribe.get('http://httpbin.org/ip', proxies = tor)
 	print(myip.text)
 	getHeaders = windscribe.get(signup); heads = getHeaders.headers
 	post = windscribe.post(signup, data=payload)
 	
-	print(postCaptcha)
+	print(postCaptcha) #one day...
 	
 	
 windscribe.close()
