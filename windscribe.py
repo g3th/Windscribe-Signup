6aefeef8b08649d7b7606885a1c73c7e359f4205
g3th <theinkandpen@yahoo.co.uk>
2022-06-13 03:07:19 +0100

    modified:   header.py 	modified:   windscribe.py 	readimage.py 	servers.py

--- windscribe.py
+++ windscribe.py
@@ -1,25 +1,21 @@
 import requests
+import shlex
+import subprocess
 
-from generator import password_generator
-from generator import u_gen
+from readimage import screenshot, ocr
+from servers import pickACard
+from generator import password_generator, u_gen
 from header import titleHeader
 
-email = 'youremail' #some temp-email api?
-
-signup = 'https://windscribe.com/signup'
+email = 'youremail' ; signup = 'https://windscribe.com/signup'; captcha = 'https://windscribe.com/captcha/'
 #page = 'https://res.windscribe.com/res/init'
 
-# Your own credentials here --->
+# Change 'p.gen' number to a password length of your choice --->
 
-print('\x1bc')
-
-titleHeader()
-
-print('-'*60)
+print('\x1bc'); titleHeader()
 
 p=password_generator(); u=u_gen()
 password=p.gen(10); username = u.name()
-
 
 payload = {'signup': '1','username':username,'password':password,'password2': password,'email': '','voucher_code': '','captcha': '','robert_status': '1','unlimited_plan': '0'}
 
@@ -27,12 +23,21 @@ with requests.session() as windscribe:
 	
 	# Your Ipvanish proxy credentials (not the same as login!) --->
 
-	tor = {'http':'socks5h://user:pass@tor.socks.ipvanish.com:1080'}
-	myip = windscribe.get('http://httpbin.org/ip', proxies = tor)
-	print(myip.text)
-	getHeaders = windscribe.get(signup); heads = getHeaders.headers
-	post = windscribe.post(signup, data=payload)
+	ipvanish = {'http':'socks5h://'+ pickACard('', '')}
+	myip = windscribe.get('http://httpbin.org/ip', proxies = ipvanish)
+	print('\033[38;5;236mUsername: \033[38;5;242m'+username+" \033[38;5;236m| Password: \033[38;5;242m"+password+"\033[38;5;236m | "+ "IP ----> \033[38;5;242m", myip.text.split('"')[3],"\n")
+	while True:
 	
-	print(postCaptcha) #one day...
+		getHeaders = windscribe.get(signup); heads = getHeaders.headers
+		post = windscribe.post(signup, data=payload, proxies = ipvanish)
+		print(post.text.split('"')[2])
+		preCaptcha = post.text.split('/')[2].strip('"}')
+		if ':1337' in post.text:
+			pass
+		else:
+			subprocess.run(shlex.split('wget --output-file=view.png "'+'https://windscribe.com/captcha/'+preCaptcha+'"'),shell=False)
+			break
+	#subprocess.run('
+	#print(postCaptcha) one day...
 	
 windscribe.close()
